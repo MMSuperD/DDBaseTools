a8a2e4e3a9f6605879adaca6637ee43dbcf261a4
GodSuperD <1653591454@qq.com>
2019-09-17 05:57:09 +0800

    创建了一个新项目

---
+++ DDBaseToolsDemo/DDBaseToolsDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DDBaseToolsDemo
+//
+//  Created by wangwei on 2019/9/17.
+//  Copyright © 2019 SuperD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ DDBaseToolsDemo/DDBaseToolsDemo/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  DDBaseToolsDemo
+//
+//  Created by wangwei on 2019/9/17.
+//  Copyright © 2019 SuperD. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
