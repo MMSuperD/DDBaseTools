bc03ae20bb5d43724d53bb7ce494f40f90be3cd4
GodSuperD <1653591454@qq.com>
2019-09-17 05:59:44 +0800

    创建了第一个测试文件

---
+++ DDBaseToolsDemo/DDBaseToolsDemo/TestObject.h
@@ -0,0 +1,19 @@
+//
+//  TestObject.h
+//  DDBaseToolsDemo
+//
+//  Created by wangwei on 2019/9/17.
+//  Copyright © 2019 SuperD. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface TestObject : NSObject
+
++ (void)show;
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ DDBaseToolsDemo/DDBaseToolsDemo/TestObject.m
@@ -0,0 +1,17 @@
+//
+//  TestObject.m
+//  DDBaseToolsDemo
+//
+//  Created by wangwei on 2019/9/17.
+//  Copyright © 2019 SuperD. All rights reserved.
+//
+
+#import "TestObject.h"
+
+@implementation TestObject
+
++ (void)show {
+    NSLog(@"I love you!");
+}
+
+@end
