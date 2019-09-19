bb72f02624e76c160e316449d5e11c40552b582d
zcx <648510978@qq.com>
2019-09-19 17:30:06 +0800

    创建了分类头文件

---
+++ DDBaseToolsDemo/DDBaseToolsDemo/Class/HeaderFile/CatergoryHeaderFile.h
@@ -0,0 +1,20 @@
+//
+//   FileName:  CatergoryHeaderFile.h
+//   ProjectName:  DDBaseToolsDemo
+//  Organization:  Copyright (c) Hello Technologies Co., Ltd. ____YEAR___. All rights reserves.
+//
+//  Description:
+//  Author:  Fan(王丹)
+//  Create:  2019/9/19
+//  CompanyName:  通华金科
+//  This head path is “~/Library/Developer/Xcode/UserData/IDETemplateMacros.plist”
+
+#ifndef CatergoryHeaderFile_h
+#define CatergoryHeaderFile_h
+
+#import "UIFont+adaptive.h"
+#import "UIView+propertyExtension.h"
+#import "UIColor+functionExtension.h"
+
+
+#endif /* CatergoryHeaderFile_h */
